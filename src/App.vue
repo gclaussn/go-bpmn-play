5a08aba8547fe139dbd15b52bb5d113fc9c73e67
Gabor Claussnitzer <gabor.claussnitzer@googlemail.com>
2026-05-04 17:27:49 +0200

    feat: enable fetching of BPMM XML from URL

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <script setup>
-import { computed } from "vue"
+import { computed, onBeforeMount } from "vue"
 
-import { bpmnXml } from "./state.js"
+import { bpmnXml, bpmnXmlError } from "./state.js"
 
 import BpmnChooser from "./BpmnChooser.vue"
 import Play from "./Play.vue";
+
+onBeforeMount(async () => {
+  if (!window.location.search) {
+    return
+  }
+
+  const searchParams = new URLSearchParams(window.location.search)
+
+  const fetchParam = searchParams.get("fetch")
+  if (!fetchParam) {
+    return
+  }
+
+  try {
+    const response = await fetch(fetchParam)
+    if (!response.ok) {
+      bpmnXmlError.value = `HTTP ${response.status} when fetching ${fetchParam}`
+      bpmnXml.value = null
+      return
+    }
+
+    bpmnXml.value = await response.text()
+  } catch (err) {
+    bpmnXmlError.value = err
+    bpmnXml.value = null
+  }
+})
 
 const current = computed(() => {
   if (bpmnXml.value) {
